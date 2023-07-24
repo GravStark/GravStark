00ac97d4c51a05eb9e844286fc8b67fca8a4405c
Grav <133029304+GravStark@users.noreply.github.com>
2023-07-24 14:27:15 +0200

    Create GravStark.bat

---
+++ GravStark.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set name=@GravStark
+set nationality=Italian
+set age=18
+set city=Modena
+
+echo Hi, I'm %name%, an %nationality% %age% y/o student from %city%.
+echo ✨ Interests:
+echo - Programming
+echo - VideoGames
+echo - Music
+echo - Healthcare
+echo ✨ Languages:
+echo - Italian
+echo - English
+echo - Finnish
+echo ✨ Programming languages:
+echo - C
+echo - C++
+echo - C#
+echo - Batch
+echo - Assembly
+echo - Java
+echo - Html
+echo - CSS
+echo - JavaScript
